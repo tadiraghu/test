cf0cef37cbadc5bace22a82f1319d855d89110d3
tadiraghu <raghu@infomile.com>
2016-11-11 17:35:52 +0530

    test commit

---
+++ testapp/testapp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(testapp.Startup))]
+namespace testapp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
